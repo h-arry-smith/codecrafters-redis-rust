f00c55e7ee8539a7077a7df1167f60a113d1dad5
Harry Smith <harry@hsmith.net>
2023-10-30 13:05:46 +0800

    Tokio message passing event loop type thing

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,79 @@
-use anyhow::{Context, Result};
-use std::{
-    io::{Read, Write},
+use anyhow::Result;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
+    sync::{
+        mpsc::{self, Sender},
+        oneshot,
+    },
 };
 
-fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+#[derive(Debug)]
+enum Command {
+    Ping { resp: oneshot::Sender<String> },
+}
+
+async fn handle_connection(stream: &mut TcpStream, tx: Sender<Command>) {
     loop {
         let mut buffer = [0; 1024];
-        let read_amount = stream
-            .read(&mut buffer)
-            .context("Failed to read from stream")?;
+        let read_amount = stream.read(&mut buffer).await.unwrap();
+
+        eprintln!(
+            "Read {} bytes for stream {}",
+            read_amount,
+            stream.local_addr().unwrap()
+        );
 
         if read_amount == 0 {
             break;
         }
 
-        stream
-            .write_all(b"+PONG\r\n")
-            .context("Failed to write to stream")?;
-    }
+        let (resp_tx, resp_rx) = oneshot::channel();
 
-    Ok(())
+        tx.send(Command::Ping { resp: resp_tx }).await.unwrap();
+        eprintln!("send ping command over oneshot");
+
+        let response = resp_rx.await.unwrap();
+        eprintln!("received response over oneshot");
+        stream.write_all(response.as_bytes()).await.unwrap();
+        eprintln!("wrote response to stream {}", stream.peer_addr().unwrap());
+    }
 }
 
-fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() -> Result<()> {
+    let (tx, mut rx) = mpsc::channel(32);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                handle_connection(&mut stream)?;
-            }
-            Err(e) => {
-                println!("error: {}", e);
+    let server = tokio::spawn(async move {
+        let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+        eprintln!("Listening on {}", listener.local_addr().unwrap());
+
+        loop {
+            let mut handles = Vec::new();
+            let (mut stream, _) = listener.accept().await.unwrap();
+            eprintln!("Accepted connection from {}", stream.peer_addr().unwrap());
+
+            let task_tx = tx.clone();
+            handles.push(tokio::spawn(async move {
+                handle_connection(&mut stream, task_tx).await;
+            }));
+        }
+    });
+
+    let message_handler = tokio::spawn(async move {
+        while let Some(cmd) = rx.recv().await {
+            println!("Received command over mpsc: {:?}", cmd);
+            match cmd {
+                Command::Ping { resp } => {
+                    println!("Sending pong over oneshot");
+                    resp.send("+PONG\r\n".to_string()).unwrap();
+                }
             }
         }
-    }
+    });
+
+    server.await.unwrap();
+    message_handler.await.unwrap();
 
     Ok(())
 }
