8ed750d05ea553d51fbcaf64f517f953b61a7f46
Harry Smith <harry@hsmith.net>
2023-10-30 12:01:44 +0800

    Dumb respond to PING with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use std::net::TcpListener;
+use anyhow::{Context, Result};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
+fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+    let mut buffer = [0; 1024];
+    stream
+        .read(&mut buffer)
+        .context("Failed to read from stream")?;
+
+    stream
+        .write_all(b"+PONG\r\n")
+        .context("Failed to write to stream")?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                handle_connection(&mut stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
